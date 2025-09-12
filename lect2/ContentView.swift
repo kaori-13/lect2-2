1db2f5925cd147ee4edc1ade3d2a8c2e7be35f23
Fanny <galaxysgra@gmail.com>
2025-09-12 09:30:20 +0800

    Initial Commit

---
+++ lect2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lect2
+//
+//  Created by Fanny on 2025/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lect2/lect2App.swift
@@ -0,0 +1,17 @@
+//
+//  lect2App.swift
+//  lect2
+//
+//  Created by Fanny on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct lect2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
